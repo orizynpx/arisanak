07ca6bb79d64561cbf20a9244be4973280ba395b
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-30 11:58:38 +0800

    feat: implement MainActivity for application entry point

---
+++ app/src/main/java/io/github/naupyon/arisanak/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.github.naupyon.arisanak.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.naupyon.arisanak.presentation.ui.ArisanApp
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArisanApp()
+        }
+    }
+}
