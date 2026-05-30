6643df3b008df9c0767f65f76f883810c0ae88be
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-30 11:57:00 +0800

    feat: add Member data class for representing member details

---
+++ app/src/main/java/io/github/naupyon/arisanak/domain/model/Member.kt
@@ -0,0 +1,11 @@
+package io.github.naupyon.arisanak.domain.model
+
+data class Member(
+    val id: Long = 0,
+    val groupId: Long?,
+    val contactId: String?,
+    val displayName: String,
+    val phoneNumber: String?,
+    val customDueAmount: Double?,
+    val hasWon: Boolean = false
+)
